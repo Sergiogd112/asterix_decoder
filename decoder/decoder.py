61417d9cd0ad9eb2e7a43082a9b1eaa7332eb415
Greg3001 <111346504+Greg3001@users.noreply.github.com>
2025-10-08 17:25:51 +0200

    Add files via upload

--- decoder/decoder.py
+++ decoder/decoder.py
@@ -2,9 +2,8 @@ import numpy as np
 import pandas as pd
 import bitstring
 from tqdm import tqdm
-from rich import print
-from multiprocessing import Pool, cpu_count
 from .cat48 import decode_cat48
+from .cat21 import decode_cat21
 
 
 class Decoder:
@@ -16,7 +15,7 @@ class Decoder:
         current_pos = 0
         total_bits = len(bit_data)
 
-        with tqdm(total=total_bits // 8, desc="Splitting", unit="B") as pbar:
+        with tqdm(total=total_bits // 8, desc="Decoding") as pbar:
             while current_pos + 24 <= total_bits:  # Need at least 24 bits for header
                 cat = bit_data[current_pos : current_pos + 8].uint
                 length = bit_data[current_pos + 8 : current_pos + 24].uint
@@ -33,39 +32,23 @@ class Decoder:
 
         return result
 
-    @staticmethod
-    def decode_element(element):
-        cat, length, data = element
-        if cat == 48:
-            return decode_cat48(cat, length, data)
-        return None
-
-    def load(self, file_name, parallel=True):
+    def load(self, file_name):
         with open(file_name, "rb") as f:
             self.data = f.read()
         self.bit_data = bitstring.BitArray(self.data)
         print(f"Loaded {len(self.data)} bytes from {file_name}")
         self.splited_data = self.split_data(self.bit_data)
-        decoded_messages = []
+        distinct_data_items_cat48 = set()
+        distinct_data_items_cat21 = set()
+        for element in tqdm(self.splited_data[:100]):
+            cat, length, data = element
+            if cat == 48:
+                data_items_to_decode = decode_cat48(cat, length, data.bin)
+                distinct_data_items_cat48.update(data_items_to_decode)
+            elif cat == 21:
+                data_items_to_decode, decoded_data, end_pos = decode_cat21(cat, length, data)
+                distinct_data_items_cat21.update(data_items_to_decode)
 
-        if parallel:
-            with Pool(processes=min(cpu_count() - 1, 8)) as pool:
-                results = list(
-                    tqdm(
-                        pool.imap(Decoder.decode_element, self.splited_data),
-                        total=len(self.splited_data),
-                        desc="Decoding",
-                        unit="Msg",
-                    )
-                )
-        else:
-            results = list(
-                tqdm(
-                    map(Decoder.decode_element, self.splited_data),
-                    total=len(self.splited_data),
-                    desc="Decoding",
-                    unit="Msg",
-                )
-            )
-        decoded_messages.extend([msg for msg in results if msg is not None])
-        # print(decoded_messages)
+
+        print(f"Distinct CAT48 data items: {sorted(distinct_data_items_cat48)}")
+        print(f"Distinct CAT21 data items (FRNs): {sorted(distinct_data_items_cat21)}")
